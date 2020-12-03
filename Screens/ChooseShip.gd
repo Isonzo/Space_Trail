79ae328db6003fc3ee8119a528684e25169cf8a2
Packetpotatoe <60154025+Packetpotatoe@users.noreply.github.com>
2020-12-03 16:59:07 -0500

    added a screen with two buttons to choose between two different types of spaceships

---
+++ Screens/ChooseShip.gd
@@ -0,0 +1,16 @@
+extends Control
+
+
+func _on_Button_mouse_entered() -> void:
+	$VBoxContainer/Description.text = "An efficient ship that requires less fuel, but has lower capacity and bulkiness."
+
+func _on_Scout_mouse_exited() -> void:
+	$VBoxContainer/Description.text =""
+
+
+func _on_Freight_mouse_entered() -> void:
+	$VBoxContainer/Description.text = "A tanky ship designed for transporting huge amounts of cargo. Not particularly fuel efficient."
+
+func _on_Freight_mouse_exited() -> void:
+	$VBoxContainer/Description.text =""
+
