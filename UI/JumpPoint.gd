50925fec1f1dab2c340bef0bc64329e34f29808b
Packetpotatoe <60154025+Packetpotatoe@users.noreply.github.com>
2020-12-06 23:29:36 -0500

    Player now moves to different JumpPoints automatically, though there is a bug where it won't finish it's route due to faulty signalling.

--- UI/JumpPoint.gd
+++ UI/JumpPoint.gd
@@ -1,6 +1,12 @@
 extends Node2D
 
-var visited = false
+
+signal in_range
 
 func _ready() -> void:
 	pass
+
+
+func _on_Area2D_body_entered(body):
+	if body.name == "Player":
+		emit_signal("in_range")
--- UI/Travel.gd
+++ UI/Travel.gd
@@ -1,18 +1,53 @@
 extends Node2D
 
+onready var JumpPoints = $JumpPoints.get_children()
+
+var player_moving = false
+
+export var SPEED = 10
 
 func _ready() -> void:
-	
+	$JumpPoints.get_children()[0].connect("in_range", self, "_when_in_range")
+	print(JumpPoints)
 	pass
+	
+func _process(delta: float) -> void:
+#	var direction = direction_to_point()
+#	if player_moving:
+#		$Player.move_and_slide(direction * SPEED)
+#	else:
+#		$Player.move_and_slide(Vector2.ZERO)
+	pass
+
 
 func _on_Button_toggled(button_pressed: bool) -> void:
 	if (button_pressed):
+		player_moving = true
 		$Timer.start()
+
+		
 	else:
 		$Timer.stop()
+		player_moving = false
+		$Player.move_and_slide(Vector2.ZERO)
 
 
 func _on_Timer_timeout() -> void:
 	ShipInformation.daily_calculations()
 	print("A day has passed")
+	var direction = direction_to_point()
+	if player_moving:
+		$Player.move_and_slide(direction * SPEED)
 	pass # Replace with function body.
+
+func direction_to_point():
+	var direction = $Player.position.direction_to(JumpPoints[0].position)
+	return direction
+func distance_to_point():
+	var distance = $Player.position.distance_to(JumpPoints[0].position)
+	return distance
+	
+
+func _when_in_range():
+	print("Signal Worked!")
+	JumpPoints.pop_front()
