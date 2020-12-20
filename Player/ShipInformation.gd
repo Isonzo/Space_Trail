84fe5cdee4e8f0cc817250933f1e9460469e0bad
Isonzo <60154025+Isonzo@users.noreply.github.com>
2020-12-19 22:42:28 -0500

    refactored to use dictionaries rather than lists

--- Player/ShipInformation.gd
+++ Player/ShipInformation.gd
@@ -5,32 +5,46 @@ const OX_RATE = 1
 
 # A ships stats in order: HP, Fuel, Fuel use rate, Food, Oxygen %, Capacity, Crew
 var ships = {
-			"scout" : [10, 20.0, 1.0, 20.0, 100.0, 60, 3],
-			"freight" : [25, 60.0, 1.2, 50.0, 100.0, 160, 4]
-				 }
+	"scout":{
+		"hp": 10,
+		"fuel": 20.0,
+		"fuel_rate": 1.0,
+		"food": 20.0,
+		"capacity": 60,
+		"crew": 3
+		},
+	"freight":{
+		"hp": 25,
+		"fuel": 60.0,
+		"fuel_rate": 1.2,
+		"food": 50.0,
+		"capacity": 160,
+		"crew": 4
+		}
+
+	 }
 
 var ship_type = "scout"
 
-var hp = ships[ship_type][0]
-var fuel = ships[ship_type][1]
-var fuel_rate = ships[ship_type][2]
-var food = ships[ship_type][3]
-var oxygen = ships[ship_type][4]
-var capacity = ships[ship_type][5]
-var crew = ships[ship_type][6]
+var hp = ships[ship_type]["hp"]
+var fuel = ships[ship_type]["fuel"]
+var fuel_rate = ships[ship_type]["fuel_rate"]
+var food = ships[ship_type]["food"]
+var oxygen = 100.0
+var capacity = ships[ship_type]["capacity"]
+var crew = ships[ship_type]["crew"]
 var morale = 10
 
 var starvation = 0
 
 
 func init_ship_stats():
-	hp = ships[ship_type][0]
-	fuel = ships[ship_type][1]
-	fuel_rate = ships[ship_type][2]
-	food = ships[ship_type][3]
-	oxygen = ships[ship_type][4]
-	capacity = ships[ship_type][5]
-	crew = ships[ship_type][6]
+	var hp = ships[ship_type]["hp"]
+	var fuel = ships[ship_type]["fuel"]
+	var fuel_rate = ships[ship_type]["fuel_rate"]
+	var food = ships[ship_type]["food"]
+	var capacity = ships[ship_type]["capacity"]
+	var crew = ships[ship_type]["crew"]
 
 func daily_calculations():
 	food_update()
